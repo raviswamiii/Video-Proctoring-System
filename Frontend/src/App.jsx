3e9992c090791b59e5bc6cc17b888ce909e02098
raviswamiii <raviswamisamsung@gmail.com>
2025-09-14 11:26:38 +0530

    routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Interview } from './pages/Interview'
+import { Report } from './components/Report'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/interview' element={<Interview/>} />
+        <Route path='/report' element={<Report/>} />
+      </Routes>
+    </div>
   )
 }
