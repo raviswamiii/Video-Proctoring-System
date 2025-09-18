55b755b4a87ae7ca629ce31c2a9a02c230d09baf
raviswamiii <raviswamisamsung@gmail.com>
2025-09-18 11:26:31 +0530

    home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { LogIn } from './pages/LogIn'
+import { Register } from './pages/Register'
 
 export const App = () => {
   return (
-    <div className='bg-green-400'>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/logIn' element={<LogIn/>} />
+        <Route path='/register' element={<Register/>} />
+      </Routes>
+    </div>
   )
 }
