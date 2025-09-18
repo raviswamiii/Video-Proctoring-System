a8e53c852f45e4047a5c28ac22e16c9cff79b47e
raviswamiii <raviswamisamsung@gmail.com>
2025-09-18 10:24:27 +0530

    FrontendSetUp

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,7 @@
-
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Interview } from './pages/Interview'
-import { Report } from './components/Report'
-import { UserRegister } from './components/userRegister'
-import { UserLogin } from './components/userLogin'
 
 export const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/interview' element={<Interview/>} />
-        <Route path='/report' element={<Report/>} />
-        <Route path='/register' element={<UserRegister/>} />
-        <Route path='/login' element={<UserLogin/>} />
-      </Routes>
-    </div>
+    <div>App</div>
   )
 }
