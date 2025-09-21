16e3330b4d59aae91c92869152844113d5f21b63
raviswamiii <raviswamisamsung@gmail.com>
2025-09-21 11:34:12 +0530

    blacklistModel

---
+++ Backend/models/blacklistToken.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const blacklistTokenScheam = new mongoose.Schema({
+    token: {type: String, required: true, unique: true},
+    createdAt: {type: Date, default: Date.now(), expires: 86400}
+},{timestamps: true});
+
+const blacklistTokenModel = mongoose.models.blacklistToken || mongoose.model("blacklistToken", blacklistTokenScheam);
+
+export default blacklistTokenModel;
